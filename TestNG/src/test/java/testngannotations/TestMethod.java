84d3005544f162f88ddff07fc8455faf836777ac
Gowri <Gowri@192.168.1.3>
2025-09-10 19:27:14 +0530

    created @Test method

---
+++ TestNG/src/test/java/testngannotations/TestMethod.java
@@ -0,0 +1,13 @@
+package testngannotations;
+
+import org.testng.annotations.Test;
+
+public class TestMethod {
+
+	@Test
+	public void testLogin() {
+		System.out.println("User logged in successfully");
+
+	}
+
+}
