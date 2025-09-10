5814a673127ac4df64782bc3fd6342c1ff854427
Gowri <Gowri@192.168.1.3>
2025-09-10 19:46:33 +0530

    created @BeforeClass and @AfterClass annotation

--- TestNG/src/test/java/testngannotations/BeforeAfterClass.java
+++ TestNG/src/test/java/testngannotations/BeforeAfterClass.java
@@ -2,19 +2,12 @@ package testngannotations;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
 public class BeforeAfterClass {
 
 	@BeforeClass
 	public void launchBrowser() {
 		System.out.println("Launched Chrome Browser");
-
-	}
-
-	@Test
-	public void testLogin() {
-		System.out.println("User logged in successfully");
 
 	}
 
